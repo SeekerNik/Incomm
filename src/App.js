b8c443dac791881706c36dfaabc040bb4ac6ef71
nik6798 <nikhilsaini94169@gmail.com>
2020-06-28 14:34:58 +0530

    Creating the ecom project for the first time

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Homepage from "./components/Homepage.component"
+
+function App() {
+  return <div><Homepage/></div>;
+}
+
+export default App;

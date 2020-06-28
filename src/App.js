859766a6e0a0ced2d565b8132c45ef47ed7bb57e
nik6798 <nikhilsaini94169@gmail.com>
2020-06-28 16:30:47 +0530

    organise component into folders and adding images and menu-item and other stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./components/Homepage.component"
+import Homepage from "./pages/homepage/Homepage.component"
 
 function App() {
   return <div><Homepage/></div>;
---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './homepage.styles.scss'
+import Menu from "../../components/menu/menu.component";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Menu />
+    </div>
+  );
+};
+
+export default Homepage;

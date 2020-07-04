235db87c6814ccaa477edd131af260e22ab9f06d
nik6798 <nikhilsaini94169@gmail.com>
2020-07-04 16:32:27 +0530

    adding routing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./App.css";
-import Homepage from "./pages/homepage/Homepage.component"
+import {Route} from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage.component";
+
+function Hatpage() {
+  return (
+    <div>
+      <h1>Hat's pages</h1>
+    </div>
+  );
+}
 
 function App() {
-  return <div><Homepage/></div>;
+  return (
+    <div>
+      <Route exact path="/" component={Homepage} />
+      <Route exact path="/shop/hatpage" component={Hatpage} />
+    </div>
+  );
 }
 
 export default App;
